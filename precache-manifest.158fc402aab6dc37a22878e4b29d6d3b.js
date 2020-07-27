4f686361932bb8bc2fbfef9ca105086a696e22a2
Divyanshi Singh <singhdivyanshi797@gmail.com>
2020-07-28 03:57:53 +0530

    Updates

---
+++ precache-manifest.158fc402aab6dc37a22878e4b29d6d3b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfc36a4110e08fb8a3f4996d4a723a3",
+    "url": "/quizz/index.html"
+  },
+  {
+    "revision": "4637b5de112faea6638b",
+    "url": "/quizz/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a312e8a99e704eb91e80",
+    "url": "/quizz/static/css/main.f75fa68c.chunk.css"
+  },
+  {
+    "revision": "4637b5de112faea6638b",
+    "url": "/quizz/static/js/2.d250fb1c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/quizz/static/js/2.d250fb1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a312e8a99e704eb91e80",
+    "url": "/quizz/static/js/main.408a7a9a.chunk.js"
+  },
+  {
+    "revision": "9fbbb25251784bcea0b6",
+    "url": "/quizz/static/js/runtime-main.b8bddf99.js"
+  },
+  {
+    "revision": "d8f73007b013c4a035a3e989890d2efc",
+    "url": "/quizz/static/media/fertility-pic.d8f73007.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quizz/static/media/logo.5d5d9eef.svg"
+  }
+]);
